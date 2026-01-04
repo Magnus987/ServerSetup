b0badf9850eb027ef72ab6823ab5989a40b90156
magnus <magnusvonwachter@gmail.com>
2026-01-04 15:23:13 +0100

    minor changes

--- setup.sh
+++ setup.sh
@@ -8,5 +8,13 @@ if ! command -v ansible &> /dev/null; then
     sudo apt install -y ansible
 fi
 
+if ! command -v netaddr &> /dev/null; then
+    echo "Installing netaddr..."
+    sudo apt update
+    sudo apt install -y python3-netaddr
+fi
+
+# install role for playbook
+ansible-galaxy install nusenu.relayor
 # execute playbook
 ansible-playbook playbook.yml
